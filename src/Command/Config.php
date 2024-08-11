e5a62d60664e846f61db2eaea95d0f748f8269d3
Tereta Alexander <atereta@gomage.com>
2024-08-11 13:45:59 +0300

    Release: 0.0.17

--- src/Command/Config.php
+++ src/Command/Config.php
@@ -8,7 +8,23 @@ use Framework\Cli\Symbol;
 use Framework\Helper\Config as ConfigHelper;
 
 /**
+ * ···························WWW.TERETA.DEV······························
+ * ·······································································
+ * : _____                        _                     _                :
+ * :|_   _|   ___   _ __    ___  | |_    __ _        __| |   ___  __   __:
+ * :  | |    / _ \ | '__|  / _ \ | __|  / _` |      / _` |  / _ \ \ \ / /:
+ * :  | |   |  __/ | |    |  __/ | |_  | (_| |  _  | (_| | |  __/  \ V / :
+ * :  |_|    \___| |_|     \___|  \__|  \__,_| (_)  \__,_|  \___|   \_/  :
+ * ·······································································
+ * ·······································································
+ *
  * @class Framework\Application\Command\Config
+ * @package Framework\Application\Command
+ * @link https://tereta.dev
+ * @since 2020-2024
+ * @license   http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ * @author Tereta Alexander <tereta.alexander@gmail.com>
+ * @copyright 2020-2024 Tereta Alexander
  */
 class Config implements Controller
 {
